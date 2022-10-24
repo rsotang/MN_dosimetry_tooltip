941fdb0ee0f8bd4e20f2a47c7c8bef3ca77b2087
rsota <rsotang@gmail.com>
2022-10-24 20:34:49 +0200

    función rapida para poder leer nombres de variables ya que matlab no deja

---
+++ getVarName.m
@@ -0,0 +1,7 @@
+%tengo que hacer una función que me de el nombre de las variables en string
+%porque MATLAB no funciona como python y no puedo acceder a las propiedades
+%de las variables así como así
+
+function out = getVarName(var)
+    out = inputname(1);
+end
